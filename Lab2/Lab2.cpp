10ed8f3979eeb8b22d554d52d6e867ae4447c195
Jackson Randolph <agent00jackson@gmail.com>
2019-06-12 20:41:27 -0400

    Compiling, added dynList class

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -3,57 +3,115 @@
 #include <vector>
 #include <cstdlib> 
 #include <ctime>
+#include <iterator>
+#include <algorithm>
 
+#define templ template<class T>
 
+templ DynList<T>::DynList()
+{
+    this->Length = 50;
+    this->dynArr = new T*[Length];
+}
 
-proc::proc(std::vector<proc*>& procList)
+templ DynList<T>::~DynList()
+{
+    delete[] dynArr;
+}
+
+templ int DynList<T>::GetLength()
+{
+    return this->Length;
+}
+
+templ void DynList<T>::SetElement(T* obj, int pos)
+{
+    if(pos < this->Length)
+    {
+        dynArr[pos] = obj;
+    }
+}
+
+templ T* DynList<T>::GetElement(int pos)
+{
+    if(pos >= Length)
+    {
+        throw "Index out of range!";
+    }
+
+    return dynArr[pos];
+}
+
+templ void DynList<T>::Push(T* obj)
+{
+    for(int i = 0; i < Length; i++)
+    {
+        if(this->dynArr == NULL)
+        {
+            this->dynArr[i] = obj;
+            return;
+        }
+    }
+    this->Expand();
+    this->dynArr[Length] = obj;
+}
+
+templ void DynList<T>::Expand()
+{
+    int newLength = Length + 25;
+    T** newArr = new T*[newLength];
+    std::copy(dynArr, dynArr+Length, newArr);
+}
+
+Proc::Proc(std::vector<Proc*>& ProcList)
 {
     //Initialize Values
-    this->procID = (int)this;
-    this->PriorityNumber = this->procID;
-    this->Enlist(procList);
+    this->ProcID = (int)this;
+    this->PriorityNumber = this->ProcID;
+    this->Enlist(ProcList);
+    
 }
 
-proc::~proc()
+Proc::~Proc()
 {
-    std::cout << "proc: " << this->procID << " deconstructed!" << std::endl;
+    std::cout << "Proc: " << this->ProcID << " deconstructed!" << std::endl;
 }
 
-void proc::Enlist(std::vector<proc*>& procList)
+void Proc::Enlist(std::vector<Proc*>& ProcList)
 {
-    procList.push_back(this);
-    std::cout << "proc: " << this->procID << " enlisted." << std::endl;
+    ProcList.push_back(this);
+    std::cout << "Proc: " << this->ProcID << " enlisted." << std::endl;
 }
 
-void proc::Delist(std::vector<proc*>& procList)
+void Proc::Delist(std::vector<Proc*>& ProcList)
 {
-    std::cout << "proc " << this->procID << " not found." << std::endl;
+    std::cout << "Proc " << this->ProcID << " not found." << std::endl;
     return;
 }
 
-void proc::Reset_Priority()
+void Proc::Reset_Priority()
 {
-    std::cout << "proc: " << this->procID << " priority has been reset." << std::endl;
+    std::cout << "Proc: " << this->ProcID << " priority has been reset." << std::endl;
 }
 
-double proc::Time_In_System()
+double Proc::Time_In_System()
 {
-    std::cout << "proc: " << this->procID << " has been in the system for x milliseconds" << std::endl;
+    std::cout << "Proc: " << this->ProcID << " has been in the system for x milliseconds" << std::endl;
     return this->ArrivalTime;
 }
 
-double proc::Waiting_Time()
+double Proc::Waiting_Time()
 {
-    std::cout << "proc: " << this->procID << " has been waiting for x milliseconds" << std::endl;
+    std::cout << "Proc: " << this->ProcID << " has been waiting for x milliseconds" << std::endl;
     return this->ServiceTime;
 }
 
 int main()
 {
-    std::vector<proc*> procList = std::vector<proc*>();
-    proc* test = new proc(procList);
-    test->Enlist(procList);
-    test->Delist(procList);
+    std::vector<Proc*> ProcList = std::vector<Proc*>();
+    Proc* test = new Proc(ProcList);
+    test->Enlist(ProcList);
+    test->Delist(ProcList);
     test->Reset_Priority();
     test->Waiting_Time();
     test->Time_In_System();
--- Lab2/Lab2.h
+++ Lab2/Lab2.h
@@ -1,19 +1,35 @@
 #include <vector>
 
-class proc
+class Proc
 {
     public:
-        int procID;
+        int ProcID;
         int PriorityNumber;
         double ArrivalTime;
         double ServiceTime;
-        void Enlist(std::vector<proc*>&);
-        void Delist(std::vector<proc*>&);
+        void Enlist(std::vector<Proc*>&);
+        void Delist(std::vector<Proc*>&);
         void Reset_Priority();
         double Waiting_Time();
         double Time_In_System();
-        proc(std::vector<proc*>&);
-        ~proc();
+        Proc(std::vector<Proc*>&);
+        ~Proc();
     private:
         int QueueSize;
+};
+
+template <class T>
+class DynList
+{
+    private:
+        T** dynArr;
+        int Length;
+        void Expand();
+    public:
+        int GetLength();
+        T* GetElement(int);
+        void SetElement(T*, int);
+        void Push(T*);
+        DynList();
+        ~DynList();
 };
